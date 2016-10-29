26212d95924c35fc921aebd54b6a8db75f490ede
Vitor <vitorvqz@gmail.com>
2016-10-28 21:41:07 -0300

    Reiniciando Repositorio

--- functions/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include('../config/config.php');
-	$session_uid='';
-	$_SESSION['uid']=''; 
-	if(empty($session_uid) && empty($_SESSION['uid'])){
-		header("Location: ../index.php");
-		//echo "<script>window.location='$url'</script>";
-	}
-
